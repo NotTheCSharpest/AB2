48aed2b1f00b9c51f5d47f041ea238e9696d0193
Soshi <soshi.mimicry@gmail.com>
2020-04-03 10:56:27 +0200

    Folloewd Aoba's suggestions, Pascalcase/Camelcase, decimal->int for dict., listing menuitems

--- MenuOrder.cs
+++ MenuOrder.cs
@@ -4,7 +4,7 @@ namespace AB2
 {
     public class MenuOrder
     {
-        public IDictionary<MenuItem, decimal> cart = new Dictionary<MenuItem, decimal>();
+        public IDictionary<MenuItem, int> cart = new Dictionary<MenuItem, int>();
 
         public void Add(MenuItem itemchoice)
         {
@@ -12,35 +12,21 @@ namespace AB2
             bool keyExists = cart.ContainsKey(itemchoice);
             if (keyExists)
             {
-                decimal newQuant = cart[itemchoice] + 1M;
+                int newQuant = cart[itemchoice] + 1;
                 cart[itemchoice] = newQuant;
                 Console.WriteLine("{0} added", itemchoice.itemName);
             }
             else
             {
-                cart.Add(itemchoice, 1M);
+                cart.Add(itemchoice, 1);
                 Console.WriteLine("{0} added", itemchoice.itemName);
             }
         }
 
-        public void PrintMenu()
-        {
-            /* example from discord.
-             * foreach(var item in menuItems)
-                {
-                  Console.WriteLine(item.PropOne);
-                  Console.WriteLine(item.PropTwo);
-                  Console.WriteLine(item.PropThree);
-                  Console.WriteLine(item.PropFour);
-                  Console.WriteLine(item.PropFive);
-                  //and so on
-                }
-                */
-        }
         public void PrintOrder()
         {
             Console.WriteLine("\r\nprinting order...");
-            foreach (KeyValuePair<MenuItem, decimal> entry in cart)
+            foreach (KeyValuePair<MenuItem, int> entry in cart)
 
             {
                 decimal kosten = entry.Value *  entry.Key.itemPrice;                
@@ -52,7 +38,7 @@ namespace AB2
         public void OrderTotal()
         {
             decimal total = 0;
-            foreach (KeyValuePair<MenuItem, decimal> entry in cart)
+            foreach (KeyValuePair<MenuItem, int> entry in cart)
             {
                 decimal kosten = entry.Value * entry.Key.itemPrice;
                 
--- Program.cs
+++ Program.cs
@@ -8,28 +8,32 @@ namespace AB2
     {
         static void Main(string[] args)
         {
-        /* Menu Items herstellen */
+            /* Menu Items herstellen */
+            MenuItem[] itemsAvailable = new MenuItem[]
+        {
+            new MenuItem() {itemName = "Milchkaffee", itemPrice = 2.20M, itemType = "Getränke"},
+            new MenuItem() {itemName = "Kakao", itemPrice = 2M, itemType = "Getränke"},
+            new MenuItem() {itemName = "MineralWasser", itemPrice = 1.80M, itemType = "Getränke"},
+            new MenuItem() {itemName = "Espresso", itemPrice = 2M, itemType = "Getränke"},
+            new MenuItem() {itemName = "Espresso Macchiato", itemPrice = 2.5M, itemType = "Getränke"},
+            new MenuItem() {itemName = "Käsekuchen", itemPrice = 2.3M, itemType = "Speisen"},
+            new MenuItem() {itemName = "Blaubeermuffin", itemPrice = 1.3M, itemType = "Speisen"},
+            new MenuItem() {itemName = "Brownie", itemPrice = 1M, itemType = "Speisen"},
+            new MenuItem() {itemName = "Apfelkuchen", itemPrice = 2.4M, itemType = "Speisen"},
+            new MenuItem() {itemName = "Kirschtorte", itemPrice = 3M, itemType = "Speisen"},
+            new MenuItem() {itemName = "Taglicher Mittagstisch", itemPrice = 4.5M, itemType = "Speisen"}
+        };
+
         var order = new MenuOrder();
-        var Milchkaffee = new MenuItem() { itemName = "Milchkaffee", itemPrice = 2.20M, itemType = "Getränke"};
-        var Kakao = new MenuItem() { itemName = "Kakao", itemPrice = 2M, itemType = "Getränke" };
-        var MineralWasser = new MenuItem() { itemName = "MineralWasser", itemPrice = 1.80M, itemType = "Getränke" };
-        var Espresso = new MenuItem() { itemName = "Espresso", itemPrice = 2M, itemType = "Getränke" };
-        var LatteMacchiato = new MenuItem() { itemName = "Espresso Macchiato", itemPrice = 2.5M, itemType = "Getränke" };
-        var Kaesekuchen = new MenuItem() { itemName = "Käsekuchen", itemPrice = 2.3M, itemType = "Speisen" };
-        var Blaubeermuffin = new MenuItem() { itemName = "Blaubeermuffin", itemPrice = 1.3M, itemType = "Speisen" };
-        var Brownie = new MenuItem() { itemName = "Brownie", itemPrice = 1M, itemType = "Speisen" };
-        var Apfelkuchen = new MenuItem() { itemName = "Apfelkuchen", itemPrice = 2.4M, itemType = "Speisen" };
-        var Kirschtorte = new MenuItem() { itemName = "Kirschtorte", itemPrice = 3M, itemType = "Speisen" };
-        var Mittagstisch = new MenuItem() { itemName = "Taglicher Mittagstisch", itemPrice = 4.5M, itemType = "Speisen" };
 
         /* print menu and initial greeting */
         Console.WriteLine("Welcome to Cafe Blau");
         Console.WriteLine("--------------------");
         Console.WriteLine("--------Menu--------");
-            /* does not work currently */
-            foreach (var item in MenuItem)
+            /* print available items */
+            foreach (var menu in itemsAvailable)
             {
-                Console.WriteLine("{} {}", item.itemName, item.itemPrice);
+                Console.WriteLine("{0} - {1}", menu.itemName, menu.itemPrice);
             }
 
         /* take order*/
